38896141308a889ea351fa1fca02d62a7bb44a1f
karen23v <71695800@institutocajas.info>
2024-10-11 12:23:48 -0500

    Update ejercicio02.dart

--- ejercicio02.dart
+++ ejercicio02.dart
@@ -1,3 +1,35 @@
+// Ejercicio 02
+//Una empresa evalúa a sus empleados bajo dos criterios: puntualidad y rendimiento. En cada caso,
+//el empleado recibe un puntaje que va de 1 a 10, de acuerdo con los siguientes criterios:
+//Puntaje por puntualidad: - está en función de los minutos de tardanza de acuerdo con la
+//siguiente tabla:
+//Minutos de tardanza Puntaje
+//0 10
+//1 a 2 8
+//3 a 5 6
+//6 a 9 4
+//Mas de 9 0
+//Puntaje por rendimiento: - está en función de la cantidad de observaciones efectuadas al
+//empleado por no cumplir sus obligaciones de acuerdo con la siguiente tabla:
+//Observaciones efectuadas Puntaje
+//0 10
+//1 8
+//2 5
+//3 1
+//Mas de 3 0
+//El puntaje total del empleado es la suma del puntaje por puntualidad más el puntaje por
+//rendimiento. Basándose en el puntaje total, el empleado recibe una bonificación anual de acuerdo
+//con la siguiente tabla:
+//Puntaje total Bonificación
+//Menos de 11 S/. 2.5 por punto
+//11 a 13 S/. 5.0 por punto
+//14 a 16 S/. 7.5 por punto
+//17 a 19 S/. 10.0 por punto
+//20 S/. 12.5 por punto
+// los minutos de tardanza y el número de observaciones de un empleado, diseñe un
+//programa que determine el puntaje por puntualidad, el puntaje por rendimiento, el puntaje total
+//y la bonificación que le corresponden
+
 import 'dart:io';
 
 void main(List<String> arguments) {
